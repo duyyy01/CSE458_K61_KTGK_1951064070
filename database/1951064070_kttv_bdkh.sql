9aa3d4057d0a64435658db262926c363cbd9fdb5
John Doe <1951064070@e.tlu.edu.vn>
2022-01-09 18:02:35 +0700

    upcode

---
+++ database/1951064070_kttv_bdkh.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Máy chủ: 127.0.0.1
+-- Thời gian đã tạo: Th1 09, 2022 lúc 12:00 PM
+-- Phiên bản máy phục vụ: 10.4.21-MariaDB
+-- Phiên bản PHP: 8.0.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Cơ sở dữ liệu: `1951064070_kttv_bdkh`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `duan`
+--
+
+CREATE TABLE `duan` (
+  `maduan` int(11) NOT NULL,
+  `tenduan` varchar(100) DEFAULT NULL,
+  `namthuchien` date DEFAULT NULL,
+  `linhvuc` varchar(100) DEFAULT NULL,
+  `nhiemvu` varchar(1000) DEFAULT NULL,
+  `coquanthuchien` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Đang đổ dữ liệu cho bảng `duan`
+--
+
+INSERT INTO `duan` (`maduan`, `tenduan`, `namthuchien`, `linhvuc`, `nhiemvu`, `coquanthuchien`) VALUES
+(3, 'Thủy điện Lai Châu', '2022-01-10', 'Thủy Điện', 'xây dựng đập thủy điện', 'trung tâm Ứng phó biến đổi khí hậu'),
+(7, 'Thủy điện Hòa ttanson nha', '2002-11-02', 'Thủy Điện', 'Xây dựng đập thủy điện', 'Công ty TTHH Hoa Lôi');
+
+--
+-- Chỉ mục cho các bảng đã đổ
+--
+
+--
+-- Chỉ mục cho bảng `duan`
+--
+ALTER TABLE `duan`
+  ADD PRIMARY KEY (`maduan`);
+
+--
+-- AUTO_INCREMENT cho các bảng đã đổ
+--
+
+--
+-- AUTO_INCREMENT cho bảng `duan`
+--
+ALTER TABLE `duan`
+  MODIFY `maduan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
